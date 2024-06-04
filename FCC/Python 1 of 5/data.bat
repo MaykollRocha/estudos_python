6e1d7abadce98bbb8661cdccb088e38d96db419d
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-06-03 22:47:40 -0230

    Update data.bat

--- FCC/Python 1 of 5/data.bat
+++ FCC/Python 1 of 5/data.bat
@@ -2,41 +2,7 @@
   "nome":"Projeto do Formato Aritmédico",
   "infos": {"atividade":"Build an Arithmetic Formatter Project","dia":"26 de Maio de 2024"},
   "descrição":"""
-Os estudantes do ensino fundamental muitas vezes organizam problemas aritméticos verticalmente para torná-los mais fáceis de resolver. Por exemplo, "235 + 52" se torna:  
-
-  235+  52-----:
-
-Termine a função arithmetic_arranger que recebe uma lista de strings que são problemas aritméticos, e retorna os problemas organizados verticalmente e lado a lado. A função deve opcionalmente receber um segundo argumento. Quando o segundo argumento é definido como True, as respostas devem ser exibidas.  
-Exemplo    
-Chamada da Função:  
-
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])  
-Saída:  
-   32      3801      45      123  
-+ 698    -    2    + 43    +  49  
------    ------    ----    -----  
-Chamada da Função:  
-
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
-Saída:  
-  32         1      9999      523  
-+  8    - 3801    + 9999    -  49  
-----    ------    ------    -----  
-  40     -3800     19998      474  
-Regras  
-A função retornará a conversão correta se os problemas fornecidos estiverem formatados corretamente, caso contrário, retornará uma string que descreve um erro significativo para o usuário.  
-
-Situações que retornarão um erro:  
-Se houver muitos problemas fornecidos para a função. O limite é de cinco, qualquer coisa além disso retornará: 'Erro: Muitos problemas.'  
-Os operadores apropriados que a função aceitará são adição e subtração. Multiplicação e divisão retornarão um erro. Outros operadores não mencionados neste ponto de lista não precisarão ser testados. O erro retornado será: "Erro: O operador deve ser '+' ou '-'."  
-Cada número (operando) deve conter apenas dígitos. Caso contrário, a função retornará: 'Erro: Os números devem conter apenas dígitos.'  
-Cada operando (também conhecido como número de cada lado do operador) tem um máximo de quatro dígitos de largura. Caso contrário, a string de erro retornada será: 'Erro: Os números não podem ter mais de quatro dígitos.'  
-Se o usuário fornecer o formato correto dos problemas, a conversão que você retornar seguirá estas regras:  
-Deve haver um único espaço entre o operador e o maior dos dois operandos; o operador estará na mesma linha que o segundo operando; ambos os operandos estarão na mesma ordem que fornecido (o primeiro será o superior e o segundo será o inferior).  
-Os números devem ser alinhados à direita.  
-Deve haver quatro espaços entre cada problema.  
-Deve haver traços na parte inferior de cada problema. Os traços devem percorrer todo o comprimento de cada problema individualmente. (O exemplo acima mostra como isso deve parecer.)"  
-               """,
+ """,
   "Código":"""
   def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
