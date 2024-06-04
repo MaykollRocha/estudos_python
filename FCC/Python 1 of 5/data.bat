5203bbffb5265c384fc010d66ed7dc3de965a96d
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-06-03 23:09:34 -0230

    Update data.bat

--- FCC/Python 1 of 5/data.bat
+++ FCC/Python 1 of 5/data.bat
@@ -1,18 +1,7 @@
 {
   "nome":"Projeto do Formato Aritmédico",
   "infos": {"atividade":"Build an Arithmetic Formatter Project","dia":"26 de Maio de 2024"},
-  "descrição":"""Os estudantes do ensino fundamental muitas vezes organizam problemas aritméticos verticalmente para torná-los mais fáceis de resolver. Por exemplo, "235 + 52" se torna:  
-  235\n+  52\n-----:
-Termine a função arithmetic_arranger que recebe uma lista de strings que são problemas aritméticos, e retorna os problemas organizados verticalmente e lado a lado. A função deve opcionalmente receber um segundo argumento. Quando o segundo argumento é definido como True, as respostas devem ser exibidas.  
-Exemplo    
-Chamada da Função:  
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])  
-Saída:  
-   32      3801      45      123 \n + 698    -    2    + 43    +  49\n  -----    ------    ----    -----:  
-Chamada da Função:  
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
-Saída:  
-    3801      123\n-    2    +  49\n------    -----
+  "descrição":"""Os estudantes do ensino fundamental muitas vezes organizam problemas aritméticos verticalmente para torná-los mais fáceis de resolver.
 Regras  
 A função retornará a conversão correta se os problemas fornecidos estiverem formatados corretamente, caso contrário, retornará uma string que descreve um erro significativo para o usuário.  
 Situações que retornarão um erro:  
