3b74aff2c49b39542d2bee9035a6dc24f5c8a2fb
Maykoll Rocha <107188916+MaykollRocha@users.noreply.github.com>
2024-07-26 17:20:02 -0230

    Update data.bat

--- FCC/Python 4 of 5/data.bat
+++ FCC/Python 4 of 5/data.bat
@@ -4,7 +4,7 @@
   "descrição":"""Neste projeto, você usará programação orientada a objetos para criar uma classe Retângulo e uma classe Quadrado. A classe Quadrado deve ser uma subclasse de Retângulo e herdar seus métodos e atributos.  
  """,
   "Código":"https://raw.githubusercontent.com/MaykollRocha/estudos_python/main/FCC/Python%204%20of%205/Polygon_Area_Calculator.py",
-    "imag":"output_BAFP.png",
+    "imag":"output_PAC.png",
     "agregamento":"""
                   Uma programação padrão quando você se executa qualquer curso é a produção de calculadores de area perimetro, mas essa foi muito melhor aplicada pois vem com ela os projeto de classe e como elas se comporta com hiraquia oque deixou o projeto muito mais interressante de ser feito e executado.
                   """,
